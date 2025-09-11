4b9e115f2eed63d08a07144e148052521bc57495
benjamin <ebenjaminhd11@gmail.com>
2025-09-10 22:27:39 -0500

    :)

--- backend/ldgroup/ldgroup/wsgi.py
+++ backend/ldgroup/ldgroup/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ldgroup.ldgroup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ldgroup.settings')
 
 application = get_wsgi_application()
